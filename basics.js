f24284ebfd9ec454a84333f74282b76561a2742b
Nilamadhab <nilamadhab47@gmail.com>
2022-06-18 00:38:22 +0530

    basic data types

---
+++ basics.js
@@ -0,0 +1,48 @@
+// let num =10.8;
+// console.log(Math.floor(num));
+// console.log(Math.round(num));
+// console.log(Math.random());
+
+// let num1 = "10.8";
+
+// console.log(Number(num1));
+// console.log(parseInt(num1));
+// console.log(Math.pow(2, 3));
+
+// console.log(typeof 1000n)
+
+// let str ="abcd";
+// console.log(str[2]);
+
+// console.log(`Result : ${10+20}`);
+
+// console.log(str.charAt(1));
+// console.log(str.includes("a"));
+// console.log(str.toUpperCase());
+// console.log(str.substring(1, 3));
+// console.log(str.slice(1, 2));
+// console.log(str.padStart(10, "-"))
+// console.log(str.trim())
+
+// const person = {
+//     name : "madhab",
+//     courses : "js",
+// };
+
+// console.log(JSON.stringify(person));
+// console.log(JSON.parse(JSON.stringify(person)));
+// console.log(person.name);
+
+// const map = new Map();
+// map.set(123, "hello");
+// console.log(map.get(123))
+
+// const arr = [1,2,3,4,5];
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[2])
+// console.log(arr.push(6))
+// console.log(arr)
+
+
+
